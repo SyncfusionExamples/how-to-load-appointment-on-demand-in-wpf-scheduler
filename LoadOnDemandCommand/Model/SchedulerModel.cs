3c7b70e200eee3ef2a5edf60bbb183d2424cf9d9
naveenkumar.sanjeevirayan <naveenkumar.sanjeevirayan@syncfusion.com>
2020-12-16 09:52:53 +0530

    Added LoadOnDemand samples

---
+++ LoadOnDemandCommand/Model/SchedulerModel.cs
@@ -0,0 +1,136 @@
+﻿using Syncfusion.Windows.Shared;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace LoadOnDemand_Command
+{
+    public class SchedulerModel : NotificationObject
+    {
+        DateTime from, to;
+        string eventName;
+        bool isAllDay;
+        string startTimeZone, endTimeZone;
+        Brush color;
+        private ObservableCollection<DateTime> recurrenceExceptionDates = new ObservableCollection<DateTime>();
+        private string rRUle;
+        private object recurrenceId;
+        private ObservableCollection<object> resources;
+        private string notes;
+
+        public SchedulerModel()
+        {
+        }
+
+        public DateTime From
+        {
+            get { return from; }
+            set
+            {
+                from = value;
+                RaisePropertyChanged("From");
+            }
+        }
+
+        public DateTime To
+        {
+            get { return to; }
+            set
+            {
+                to = value;
+                RaisePropertyChanged("To");
+            }
+        }
+
+        public bool IsAllDay
+        {
+            get { return isAllDay; }
+            set
+            {
+                isAllDay = value;
+                RaisePropertyChanged("IsAllDay");
+            }
+        }
+        public string EventName
+        {
+            get { return eventName; }
+            set
+            {
+                eventName = value;
+                RaisePropertyChanged("EventName");
+            }
+        }
+
+        public string Notes
+        {
+            get { return notes; }
+            set
+            {
+                notes = value;
+                RaisePropertyChanged("Notes");
+            }
+        }
+        public string StartTimeZone
+        {
+            get { return startTimeZone; }
+            set
+            {
+                startTimeZone = value;
+                RaisePropertyChanged("StartTimeZone");
+            }
+        }
+        public string EndTimeZone
+        {
+            get { return endTimeZone; }
+            set
+            {
+                endTimeZone = value;
+                RaisePropertyChanged("EndTimeZone");
+            }
+        }
+
+        public Brush Color
+        {
+            get { return color; }
+            set
+            {
+                color = value;
+                RaisePropertyChanged("Color");
+            }
+        }
+
+        public object RecurrenceId
+        {
+            get { return recurrenceId; }
+            set
+            {
+                recurrenceId = value;
+                RaisePropertyChanged("RecurrenceId");
+            }
+        }
+
+        public string RecurrenceRule
+        {
+            get { return rRUle; }
+            set
+            {
+                rRUle = value;
+                RaisePropertyChanged("RecurrenceRule");
+            }
+        }
+
+        public ObservableCollection<DateTime> RecurrenceExceptions
+        {
+            get { return recurrenceExceptionDates; }
+            set
+            {
+                recurrenceExceptionDates = value;
+                RaisePropertyChanged("RecurrenceExceptions");
+            }
+        }
+    }
+}
---
+++ LoadOnDemandEvent/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LoadOnDemand_Event
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Viewtypecombobox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var selectedValue = this.schedulerViewType.SelectedValue.ToString();
+            if (selectedValue == "TimelineDay" || selectedValue == "TimelineWeek" || selectedValue == "TimelineWorkWeek" || selectedValue == "TimelineMonth")
+            {
+                this.scheduler.DisplayDate = DateTime.Now.Date;
+            }
+            else
+                this.scheduler.DisplayDate = DateTime.Now.Date.AddHours(9);
+        }
+    }
+}
